3febf81f921bb9b8ac91cc376a2ce25364623ed5
Eugene Dlg <stayerx@gmail.com>
2023-11-11 22:06:16 +0300

    Ready for 1st review

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hwBean0 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hwBean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(hwBean0.getMessage());
+        System.out.println(hwBean0 == hwBean1);
+        Cat catBean0 = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean0.getName());
+        System.out.println(catBean0 == catBean1);
     }
 }
